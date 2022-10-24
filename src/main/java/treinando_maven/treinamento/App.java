e25715f06b802ba6943b55e04bd2a0f21de4ec05
Sidnei G. de Azevedo Júnior <sigajunior@hotmail.com>
2022-10-24 15:34:45 -0300

    Iniciando projeto

---
+++ src/main/java/treinando_maven/treinamento/App.java
@@ -0,0 +1,13 @@
+package treinando_maven.treinamento;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
